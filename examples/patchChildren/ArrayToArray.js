8765379241926ebd8e388f43ae2370e7e3590d1f
joseypan <joseypan@163.com>
2022-10-23 17:09:51 +0800

    feat: 完成更新 element 的 children - 双端对比diff 算法 （2）

--- examples/patchChildren/ArrayToArray.js
+++ examples/patchChildren/ArrayToArray.js
@@ -24,15 +24,30 @@ import { h, ref } from "../../lib/guid-mini-vue.esm.js";
 //   h("p", { key: "D" }, "D"),
 // ];
 // prev 是 C A B next是 A B
-const nextChildren = [h("p", { key: "A" }, "A"), h("p", { key: "B" }, "B")];
-const prevChildren = [
-  h("p", { key: "D" }, "D"),
-  h("p", { key: "C" }, "C"),
+// const nextChildren = [h("p", { key: "A" }, "A"), h("p", { key: "B" }, "B")];
+// const prevChildren = [
+//   h("p", { key: "D" }, "D"),
+//   h("p", { key: "C" }, "C"),
+//   h("p", { key: "A" }, "A"),
+//   h("p", { key: "B" }, "B"),
+// ];
+// prev是 A B C D E F next是 A B G C E F
+const nextChildren = [
   h("p", { key: "A" }, "A"),
   h("p", { key: "B" }, "B"),
+  h("p", { key: "G" }, "G"),
+  h("p", { key: "C", id: "next-node" }, "C"),
+  h("p", { key: "E" }, "E"),
+  h("p", { key: "F" }, "F"),
 ];
-// const nextChildren = "newChildren";
-// const prevChildren = [h("p", {}, "A"), h("p", {}, "B")];
+const prevChildren = [
+  h("p", { key: "A" }, "A"),
+  h("p", { key: "B" }, "B"),
+  h("p", { key: "C", id: "prev-node" }, "C"),
+  h("p", { key: "D" }, "D"),
+  h("p", { key: "E" }, "E"),
+  h("p", { key: "F" }, "F"),
+];
 const ArrayToArray = {
   name: "ArrayToArray",
   setup() {
